a7ca24d09df213e1758c0986626e730a0a8c0533
18mk04 <marik7939@gmail.com>
2025-12-15 14:06:20 +0530

    Add gemini client

--- backend/gemini_client.py
+++ backend/gemini_client.py
@@ -4,11 +4,11 @@ import google.generativeai as genai
 
 load_dotenv()
 
-API_KEY = os.getenv("GOOGLE_API_KEY")
+API_KEY = os.getenv("GEMINI_API_KEY")
 if not API_KEY:
     raise ValueError("Missing GEMINI_API_KEY")
 
-genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
+genai.configure(api_key=os.environ["GEMINI_API_KEY"])
 
 def generate_text(prompt: str) -> str:
     model = genai.GenerativeModel('gemini-2.5-flash')
